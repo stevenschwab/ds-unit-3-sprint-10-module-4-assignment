bffe74d23a301d9bb905c6c74216408c1b4c35b3
Steven Schwab <schwabs@umich.edu>
2025-06-18 14:58:16 -0400

    add first 3 queries

---
+++ assignment.py
@@ -0,0 +1,63 @@
+import psycopg2
+import pymongo
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from postgres_titanic_queries import *
+from mongodb_rpg_data_queries import *
+
+# load .env file
+load_dotenv()
+
+# get variables for mongodb connection
+# MONGODB_USER = os.getenv("MONGODB_USER")
+# MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+# MONGODB_DBNAME = os.getenv("MONGODB_NAME")
+
+# get variables for postgres connection
+AIVEN_DB_USER = os.getenv("AIVEN_DB_USER")
+AIVEN_DB_NAME = os.getenv("AIVEN_DB_NAME")
+AIVEN_DB_PASSWORD = os.getenv("AIVEN_DB_PASSWORD")
+AIVEN_DB_HOST = os.getenv("AIVEN_DB_HOST")
+AIVEN_DB_PORT = os.getenv("AIVEN_DB_PORT")
+
+# open connection to MongoDB
+# def create_mdb_conn(user, password, dbname, collection_name):
+#     client = pymongo.MongoClient(f"mongodb+srv://{user}:{password}@ds-unit-3-sprint-10-mod.pxenxpb.mongodb.net/{dbname}?retryWrites=true&w=majority&appName=ds-unit-3-sprint-10-module-3")
+#     # db and collection we want to connect to
+#     db = client[dbname][collection_name]
+#     return db
+
+# open connection to postgres
+pg_conn = f"postgresql+psycopg2://{AIVEN_DB_USER}:{AIVEN_DB_PASSWORD}@{AIVEN_DB_HOST}:{AIVEN_DB_PORT}/{AIVEN_DB_NAME}?sslmode=require"
+pg_engine = create_engine(pg_conn)
+
+# generic postgres query function
+def execute_postgres_query(query, engine):
+    df = pd.read_sql_query(query, engine)
+    return df
+
+# close connections
+def close_connections():
+    pg_conn.close()
+
+# main file
+if __name__ == '__main__':
+    # execute postgres queries
+    print('Executing postgres queries')
+    try:
+        print("How many passengers survived, and how many died?")
+        print(execute_postgres_query(GET_PASSENGERS_SURVIVED_AND_DIED, pg_engine))
+
+        print("How many passengers were in each class?")
+        print(execute_postgres_query(GET_PASSENGERS_IN_EACH_CLASS, pg_engine))
+
+        print("How many passengers survived/died within each class?")
+        print(execute_postgres_query(GET_PASSENGERS_SURVIVED_DIED_BY_CLASS, pg_engine))
+        
+        print('All postgres queries executed successfully')
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        pass
---
+++ postgres_titanic_queries.py
@@ -0,0 +1,27 @@
+'''Queries for postgres'''
+
+GET_PASSENGERS_SURVIVED_AND_DIED = """
+    select
+        count(*) filter (where t.survived = 1) as total_passengers_survived,
+        count(*) filter (where t.survived = 0) as total_passengers_died
+    from titanic t;
+"""
+
+GET_PASSENGERS_IN_EACH_CLASS = """
+    select
+        count(*) filter (where t.pclass = 1) as class_1,
+        count(*) filter (where t.pclass = 2) as class_2,
+        count(*) filter (where t.pclass = 3) as class_3
+    from titanic t;
+"""
+
+GET_PASSENGERS_SURVIVED_DIED_BY_CLASS = """
+    select
+        count(*) filter (where t.pclass = 1 and t.survived = 1) as class_1_survived,
+        count(*) filter (where t.pclass = 1 and t.survived = 0) as class_1_died,
+        count(*) filter (where t.pclass = 2 and t.survived = 1) as class_2_survived,
+        count(*) filter (where t.pclass = 2 and t.survived = 0) as class_2_died,
+        count(*) filter (where t.pclass = 3 and t.survived = 1) as class_3_survived,
+        count(*) filter (where t.pclass = 3 and t.survived = 0) as class_3_died
+    from titanic t;
+"""
