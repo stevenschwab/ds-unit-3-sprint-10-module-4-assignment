c28dc11cb2cb30da8b176b0c399b63200ecaafd4
Steven Schwab <schwabs@umich.edu>
2025-06-18 15:07:33 -0400

    add GET_AVG_AGE_OF_EACH_PASSENGER_CLASS query

--- assignment.py
+++ assignment.py
@@ -58,6 +58,9 @@ if __name__ == '__main__':
 
         print("What was the average age of survivors vs nonsurvivors?")
         print(execute_postgres_query(GET_AVG_AGE_OF_SURVIVORS_NONSURVIVORS, pg_engine))
+
+        print("What was the average age of each passenger class?")
+        print(execute_postgres_query(GET_AVG_AGE_OF_EACH_PASSENGER_CLASS, pg_engine))
         
         print('All postgres queries executed successfully')
     except Exception as e:
--- postgres_titanic_queries.py
+++ postgres_titanic_queries.py
@@ -32,3 +32,11 @@ GET_AVG_AGE_OF_SURVIVORS_NONSURVIVORS = """
         round(avg(t.age) filter (where t.survived = 0), 2) as avg_age_nonsurvivor
     from titanic t;
 """
+
+GET_AVG_AGE_OF_EACH_PASSENGER_CLASS = """
+    select
+        round(avg(t.age) filter (where t.pclass = 1), 2) as avg_age_pclass1,
+        round(avg(t.age) filter (where t.pclass = 2), 2) as avg_age_pclass2,
+        round(avg(t.age) filter (where t.pclass = 3), 2) as avg_age_pclass3
+    from titanic t;
+"""
